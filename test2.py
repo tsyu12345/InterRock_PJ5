681fddde5bc0e31c7a10b0a8d478313c8d13c5a5
syu_asus_Note <syukunt@icloud.com>
2021-09-20 00:20:42 +0900

    ローディングアニメーション用アイコンの追加

--- test2.py
+++ test2.py
@@ -1,28 +1,7 @@
-import time
-from multiprocessing import Pool, Process
-
-def nijou(inputs):
-    x = inputs
-    print('input: %d' % x)
-    time.sleep(2)
-    retValue = x * x
-    print('double: %d' % retValue)
-    return(retValue)
-
-if __name__ == "__main__":
-
-    # Pool()を定義
-    p = Pool()
-
-    # プロセスを2つ非同期で実行
-    result = p.apply_async(nijou, args=[3])
-    result2 = p.apply_async(nijou, args=[5])
-
-    # 1秒間隔で終了チェックして終了したら結果を表示
-    for k in range(5):
-        if result.ready():
-            break
-    print(result.get())
-    print(result2.get())
-
-    p.close()
+index = 2
+for i in range(2, 40+1):
+    index = i
+    print("HELLO")
+else:
+    print(index)
+    print("LL")
