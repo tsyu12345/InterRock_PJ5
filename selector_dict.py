94615bb90e1a7b1ee4e3d2460006087191959510
syu_asus_Note <syukunt@icloud.com>
2022-04-06 21:03:21 +0900

    セレクターの誤字修正。電話番号の抽出がNoneになる不具合を確認。また、ヘアサロンで都道府県が記録されない。

--- selector_dict.py
+++ selector_dict.py
@@ -16,9 +16,9 @@ from typing import Final as const
 SELECTOR:const[dict[str, dict[str, str]]]  = {
     "table_menu": {            
             "ヘアサロン":"#mainContents > div.mT30 > table.slnDataTbl > tbody > tr > td",
-            "ネイル・まつげサロン":"#mainContents > div.mT30 > table.wFull  > tbody > tr > th",
+            "ネイル・まつげサロン":"#mainContents > div.mT30 > table.wFull > tbody > tr > th",
             "リラクサロン":"#mainContents > div.mT30 > table.wFull > tbody > tr > th",
-            "エステサロン":"#mainContents > div:mT30 > table.wFull > tbody > tr > th",
+            "エステサロン":"#mainContents > div.mT30 > table.wFull > tbody > tr > th",
         },
         "table_value": {
             "ヘアサロン":"#mainContents > div.mT30 > table.slnDataTbl > tbody > tr > td",
